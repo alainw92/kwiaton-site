04e53e5d860a9ccf5844e39819b82c593df07f14
alainw92 <alan.wnek@gmail.com>
2022-07-30 21:02:59 +0200

    kolumny, ogrodzenia

--- src/pages/oferta/kolumny.vue
+++ src/pages/oferta/kolumny.vue
@@ -1,7 +1,11 @@
 <template>
 	<section class="pa-4">
 		<h2 class="text-h5 mb-6">Kolumny</h2>
-		<photo-gallery :folder="photosMainPath" :photos="photos"></photo-gallery>
+		<p>Posiadamy szeroki wybór wśród konfiguracji kolumn - wykonujemy wszystkie klasyczne style jak również jesteśmy otwarci na propozycje klientów.</p>
+		<h3 class="text-h6 my-6">Klasyczne głowice kolumn</h3>
+		<photo-gallery contain :folder="galleryMainPath" :photos="gallery"></photo-gallery>		
+		<h3 class="text-h6 my-6">Przykłady wykonanych projektów</h3>
+		<photo-gallery :folder="photosMainPath" :photos="photos"></photo-gallery>		
 	</section>
 </template>
 
@@ -19,18 +23,35 @@ export default {
 
 	data() {
 		return {
+			galleryMainPath: "photos/oferta/kolumny/glowice/",
+			gallery: [
+				{ src: "GD1.webp", alt: "Głowica kolumny doryckiej", title: "GD1", description: "Głowica dorycka" },
+				{ src: "GD2.webp", alt: "Głowica kolumny doryckiej", title: "GD2", description: "Głowica dorycka" },
+				{ src: "GJ1.webp", alt: "Głowica kolumny jońskiej", title: "GJ1", description: "Głowica jońska" },
+				{ src: "GJ2.webp", alt: "Głowica kolumny jońskiej", title: "GJ2", description: "Głowica jońska" },
+				{ src: "GJ3.webp", alt: "Głowica kolumny jońskiej", title: "GJ3", description: "Głowica jońska" },
+				{ src: "GK.webp", alt: "Głowica kolumny korynckiej", title: "GK", description: "Głowica koryncka" },
+			],
+
 			photosMainPath: "photos/oferta/kolumny/",
 			photos: [
-				{ src: "1.webp", alt: "Dworek", title: "Dworek", description: "Niezły dwór tam gdzie chiński mur" },
-				{ src: "2.webp", alt: "Dworek", title: "", description: "" },
-				{ src: "3.webp", alt: "Dworek", title: "", description: "" },
-				{ src: "4.webp", alt: "Dworek", title: "KL2", description: "49,5 x 50 x 23 cm" },
-				{ src: "5.webp", alt: "Dworek", title: "", description: "" },
-				{ src: "6.webp", alt: "Dworek", title: "", description: "" },
-				{ src: "7.webp", alt: "Dworek", title: "", description: "" },
-				{ src: "8.webp", alt: "Dworek", title: "", description: "" },
-				{ src: "9.webp", alt: "Dworek", title: "", description: "" },
-				{ src: "10.webp", alt: "Dworek", title: "", description: "" },
+				{ src: "1.webp", alt: "", title: "", description: "" },
+				{ src: "2.webp", alt: "", title: "", description: "" },
+				{ src: "3.webp", alt: "", title: "", description: "" },
+				{ src: "4.webp", alt: "", title: "", description: "" },
+				{ src: "5.webp", alt: "", title: "", description: "" },
+				{ src: "6.webp", alt: "", title: "", description: "" },
+				{ src: "7.webp", alt: "", title: "", description: "" },
+				{ src: "8.webp", alt: "", title: "", description: "" },
+				{ src: "9.webp", alt: "", title: "", description: "" },
+				{ src: "10.webp", alt: "", title: "", description: "" },
+				{ src: "11.webp", alt: "", title: "", description: "" },
+				{ src: "12.webp", alt: "", title: "", description: "" },
+				{ src: "13.webp", alt: "", title: "", description: "" },
+				{ src: "14.webp", alt: "", title: "", description: "" },
+				{ src: "15.webp", alt: "", title: "", description: "" },
+				{ src: "16.webp", alt: "", title: "", description: "" },
+				{ src: "17.webp", alt: "", title: "", description: "" },
 			],
 		};
 	},
--- src/pages/oferta/ogrodzenia.vue
+++ src/pages/oferta/ogrodzenia.vue
@@ -1,5 +1,8 @@
 <template>
-  <div>Ogrodzenia</div>
+  <section class="pa-4">
+		<h2 class="text-h5 mb-6">Ogrodzenia</h2>
+		<photo-gallery :folder="photosMainPath" :photos="photos"></photo-gallery>
+	</section>
 </template>
 
 <script>
@@ -13,5 +16,30 @@ export default {
 			de: "/angebot/fences",
 		},
 	},
+
+	data() {
+		return {
+			photosMainPath: "photos/oferta/ogrodzenia/",
+			photos: [
+				{ src: "1.webp", alt: "", title: "", description: "" },
+				{ src: "2.webp", alt: "", title: "", description: "" },
+				{ src: "3.webp", alt: "", title: "", description: "" },
+				{ src: "4.webp", alt: "", title: "", description: "" },
+				{ src: "5.webp", alt: "", title: "", description: "" },
+				{ src: "6.webp", alt: "", title: "", description: "" },
+				{ src: "7.webp", alt: "", title: "", description: "" },
+				{ src: "8.webp", alt: "", title: "", description: "" },
+				{ src: "9.webp", alt: "", title: "", description: "" },
+				{ src: "11.webp", alt: "", title: "", description: "" },
+				{ src: "12.webp", alt: "", title: "", description: "" },
+				{ src: "13.webp", alt: "", title: "", description: "" },
+				{ src: "14.webp", alt: "", title: "", description: "" },
+				{ src: "15.webp", alt: "", title: "", description: "" },
+				{ src: "16.webp", alt: "", title: "", description: "" },
+				{ src: "17.webp", alt: "", title: "", description: "" },
+				{ src: "18.webp", alt: "", title: "", description: "" },
+			],
+		}
+	}
 }
 </script>
